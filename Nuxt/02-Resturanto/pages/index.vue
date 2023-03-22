3e486cfe21bcf80ac1f8c868e3507208905a6746
Hussein Najah <husseinnajah123@gmail.com>
2023-03-22 19:36:51 +0300

    nuxt 2

---
+++ Nuxt/02-Resturanto/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Nav />
+        <div class="container">
+            <h1>Welcome To Restaurantly</h1>
+            <a href="/restaurants">Go To Restaurants</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    margin-top: 5rem;
+    text-align: center;
+}
+</style>
---
+++ Nuxt/02-Resturanto/pages/restaurants.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Nav />
+        <div class="container">
+            <h1>Restaurants</h1>
+            <a href="/">Go Home</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    margin-top: 5rem;
+    text-align: center;
+}
+</style>
